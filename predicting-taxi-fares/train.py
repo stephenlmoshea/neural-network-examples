70a2c219719eead85b3b7efec37096f485b3d841
Stephen O'Shea <stephenlmoshea@gmail.com>
2020-12-24 10:44:36 +0000

    adding examples

--- predicting-taxi-fares/train.py
+++ predicting-taxi-fares/train.py
@@ -16,6 +16,8 @@ from datetime import datetime
 
 from sklearn.preprocessing import MinMaxScaler
 
+from sklearn.metrics import mean_squared_error
+
 df = pd.read_csv('NYC_taxi.csv', parse_dates=['pickup_datetime'], nrows=500000)
 
 # Perform preprocessing and feature engineering
@@ -66,6 +68,49 @@ print("Training time {} minutes".format(backpropagation.getTrainingTime()/60))
 feedForward.save('./network.txt')
 
 feedForward = FeedForward.load('./network.txt')
+
+train_pred = []
+train_actual_fares = []
+train_total_rows = train.shape[0]
+
+count = 0
+for num,row in train.iterrows():
+    actual_fare = df_prescaled.loc[num,'fare_amount']
+    feedForward.activate(row[0:17].values)
+    outputs = feedForward.getOutputs()
+    predicted_fare = float(outputs[0])
+    predicted_fare_original = target_scaler.inverse_transform([[predicted_fare]])[0][0]
+    train_pred.append(predicted_fare_original)
+    train_actual_fares.append(actual_fare)
+    # print("Inside Training Set")
+    # print("{} of {}, Actual fare: {:0.2f}, Predicated fare: {:0.2f}".format(count, train_total_rows, actual_fare, predicted_fare_original))
+    count = count + 1
+
+test_pred = []
+test_actual_fares = []
+test_total_rows = test.shape[0]
+
+count = 0
+for num,row in test.iterrows():
+    actual_fare = df_prescaled.loc[num,'fare_amount']
+    feedForward.activate(row[0:17].values)
+    outputs = feedForward.getOutputs()
+    predicted_fare = float(outputs[0])
+    predicted_fare_original = target_scaler.inverse_transform([[predicted_fare]])[0][0]
+    test_pred.append(predicted_fare_original)
+    test_actual_fares.append(actual_fare)
+    # print("Inside Testing Set")
+    # print("{} of {}, Actual fare: {:0.2f}, Predicated fare: {:0.2f}".format(count, test_total_rows, actual_fare, predicted_fare_original))
+    count = count + 1
+
+
+train_rmse = np.sqrt(mean_squared_error(train_actual_fares, train_pred))
+test_rmse = np.sqrt(mean_squared_error(test_actual_fares, test_pred))
+
+print("Train RMSE: {:0.2f}".format(train_rmse))
+print("Test RMSE: {:0.2f}".format(test_rmse))
+print('------------------------')
+    
 
 def predict_random(df_prescaled, X_test, model, target_scaler):
     sample = X_test.sample(n=1, random_state=np.random.randint(low=0, high=10000))
